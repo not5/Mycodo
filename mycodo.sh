d417dd3821f223d7492fac4e58d2a078d85a4e1a
Kyle Gabriel <KyleTGabriel@gmail.com>
2019-04-30 23:55:03 -0400

    Fix dependency install, add Grafana and Telegraf

--- mycodo.sh
+++ mycodo.sh
@@ -10,23 +10,28 @@ cd ${INSTALL_PATH}
 
 case "${1:-''}" in
     "install-dependencies")
+        apt-get update -y
+        apt-get install -y python3-dev python3-setuptools libffi-dev libssl-dev
+        easy_install3 pip
+        pip install --upgrade pip
+
         printf "\n#### Checking for python 3 virtualenv at ${INSTALL_PATH}/env/bin/python3\n" 2>&1 | tee -a ${LOG_LOCATION}
         if [ ! -L ${INSTALL_PATH}/env/bin/python3 ]; then
             printf "#### Virtualenv doesn't exist. Creating...\n" 2>&1 | tee -a ${LOG_LOCATION}
             pip install virtualenv --upgrade
             rm -rf ${INSTALL_PATH}/env
             PYTHON_BINARY_SYS_LOC="$(python3.5 -c "import os; print(os.environ['_'])")"
-            virtualenv --system-site-packages -p ${PYTHON_BINARY_SYS_LOC} ${INSTALL_PATH}/env
+            virtualenv -p ${PYTHON_BINARY_SYS_LOC} ${INSTALL_PATH}/env
         else
             printf "#### Virtualenv already exists, skipping creation\n" 2>&1 | tee -a ${LOG_LOCATION}
         fi
-
-        apt install -y python3-dev libffi-dev libssl-dev
 
         if ! [ -x "$(command -v docker)" ]; then
             printf "#### Installing docker\n" 2>&1 | tee -a ${LOG_LOCATION}
             curl -sSL https://get.docker.com | sh
         fi
+
+        usermod -aG docker pi
 
         if ! [ -x "$(command -v docker-compose)" ]; then
             printf "#### Installing docker-compose\n" 2>&1 | tee -a ${LOG_LOCATION}
@@ -40,6 +45,8 @@ case "${1:-''}" in
 
         cp ./install/logrotate_docker /etc/logrotate.d/
         printf "#### Dependencies installed\n" 2>&1 | tee -a ${LOG_LOCATION}
+
+        printf "#### You must log out then back in before running 'make build'\n" 2>&1 | tee -a ${LOG_LOCATION}
     ;;
     "test")
         docker exec -ti flask ${INSTALL_PATH}/env/bin/pip install --upgrade -r /home/mycodo/mycodo/tests/software_tests/requirements.txt
